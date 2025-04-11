1e596031743ea347e0ff5606ae7e17f9b7bd38f0
Mahesh Kumar <maheshk57128@gmail.com>
2025-04-11 18:44:36 +0500

    todo app

--- app.js
+++ app.js
@@ -1,75 +1,24 @@
-var expenseList = document.getElementById("expenseList")
-    var totalExpenseEl = document.getElementById("totalExpense")
-    var totalExpense = 0
-    
-    function addExpense() {
-      var name = document.getElementById("naam").value.trim()
-      var amount =+(document.getElementById("paise").value)
 
-     var li = document.createElement("li")
-      li.innerHTML = `
-        ${name} - $${amount.toFixed(2)}
-        <button class="delete-btn" onclick="deleteExpense(this, ${amount})">Delete</button>
-      `
+var getInp = document.getElementById('inp')
+var getUl = document.getElementById('ul')
 
-      expenseList.appendChild(li)
-      totalExpense += amount
-      updateTotal()
-      document.getElementById("naam").value = ""
-      document.getElementById("paise").value = ""
-      
+function send() {
+    getUl.innerHTML += `<li> ${getInp.value} <span> <button onclick='editItem(this)' class='btn btn-info'> Edit </button> <button onclick='this.parentNode.parentNode.remove()' class='btn btn-dark'> Delete </button> </span></li>`
+    getInp.value = ''
+}
+
+function delet() {
+    getUl.innerHTML = ''
+}
+function editItem(e) {
+    var getValue = prompt("Enter updated value", e.parentNode.parentNode.firstChild.textContent)
+    e.parentNode.parentNode.firstChild.textContent = getValue
+    console.log(getValue)
+}
+
+getInp.addEventListener('keydown', function (event) {
+    var key = event.keyCode || event.which
+    if (key === 13) {
+        send()
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var expenseList = document.getElementById("expenseList")
-// var totalExpenseEl = document.getElementById("totalExpense")
-// var totalExpense = 0
-// var name = document.getElementById("naam")
-// var amount =  document.getElementById("paise")
-// function addExpense() {
-//     var li = document.createElement("li")
-//       li.innerHTML = `
-//         ${name.value} - $${amount.toFixed(2)}
-//         <button class="delete-btn" onclick="deleteExpense(this, ${amount.value})">Delete</button>
-//       `
-//       expenseList.appendChild(li)
-//       totalExpense += amount
-//       document.getElementById("naam").value = ""
-//       document.getElementById("paise").value = ""  
-// }
-// function delet() {
-//     expenseList.innerHTML = ''
-// }
+})
